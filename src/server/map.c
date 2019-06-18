9ff290544bdd26773f933c94cbfdaa58766de7df
Hugo MAURINIER <hugo.maurinier@epitech.eu>
2019-06-18 18:15:47 +0200

    [FIX] empty map generation

--- src/server/map.c
+++ src/server/map.c
@@ -17,7 +17,7 @@ mapcell_t *new_line(mapcell_t *map, int y)
     new->right = NULL;
     new->x = 0;
     new->y = y;
-    for (; tmp && tmp->down; tmp = tmp->down)
+    for (; tmp && tmp->down; tmp = tmp->down);
     new->up = tmp;
     if (!map)
         return (new);
@@ -30,27 +30,17 @@ mapcell_t *new_line(mapcell_t *map, int y)
 void new_cell(mapcell_t *map, int y, int x)
 {
     mapcell_t *new = malloc(sizeof(mapcell_t));
-    mapcell_t *tmp_line = map;
     mapcell_t *tmp_cell = map;
 
     new->down = NULL;
     new->right = NULL;
     new->x = x;
     new->y = y;
-    for (; tmp_line && tmp_line->down; tmp_line = tmp_line->down)
-        tmp_cell = tmp_cell->down;
-    tmp_cell = (tmp_cell) ? tmp_cell->down : NULL;
-    for (; tmp_cell && tmp_cell->right; tmp_cell = tmp_cell->right)
-        tmp_line = tmp_line->right;
-    if (tmp_line) {
-        tmp_line = tmp_line->right;
-        if (tmp_line)
-            tmp_line->down = new;
-        if(tmp_cell)
-            tmp_cell->right = new;
-    }
+    for (; tmp_cell && tmp_cell->down; tmp_cell = tmp_cell->down);
+    for (; tmp_cell && tmp_cell->right; tmp_cell = tmp_cell->right);
+    if (tmp_cell)
+        tmp_cell->right = new;
     new->left = tmp_cell;
-    new->up = tmp_line;
 }
 
 void check_cell(mapcell_t *cell, int x, int y, int size)
@@ -61,7 +51,7 @@ void check_cell(mapcell_t *cell, int x, int y, int size)
         printf("right\t[%d,%d]\n", x, y);
     if (y > 0 && (!cell->up))
         printf("up\t[%d,%d]\n", x, y);
-    if (x < size - 1 && (!cell->down))
+    if (y < size - 1 && (!cell->down))
         printf("down\t[%d,%d]\n", x, y);
 }
 
@@ -72,21 +62,33 @@ void check_map(mapcell_t *map, int size)
 
     for (int y = 0; line; line = line->down, y++) {
         tmp = line;
-        for (int x = 0; tmp; tmp = tmp->left, x++) {
+        for (int x = 0; tmp; tmp = tmp->right, x++) {
             check_cell(tmp, x, y, size);
+            //printf("[%d,%d] ", tmp->x, tmp->y);
         }
+        //printf("\n");
     }
 }
 
 mapcell_t *create_map(int size)
 {
     mapcell_t *map = NULL;
+    mapcell_t *save = NULL;
 
     for (int y = 0; y < size; y++) {
         map = new_line(map, y);
         for (int x = 1; x < size; x++) {
             new_cell(map, y, x);
         }
+    }
+    for (mapcell_t *prev = map; prev; prev = prev->down) {
+        save = prev;
+        for (mapcell_t *line = prev->down; line; line = line->right) {
+            prev->down = line;
+            line->up = prev;
+            prev = prev->right;
+        }
+        prev = save;
     }
     return (map);
 }
