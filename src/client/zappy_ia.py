0a5747d204c2847cee0d0ef7ca9f3a9b94ac2e55
Enzo PERROT <enzo.perrot@epitech.eu>
2019-06-20 15:50:19 +0200

    ADD.. init when connected to serv

--- src/client/zappy_ia.py
+++ src/client/zappy_ia.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 import socket
 from sys import exit
+from sys import stderr
 
 class info_ia:
     map_size_x, map_size_y = 0, 0
@@ -11,8 +12,21 @@ def check_good_start(s):
     if data != "WELCOME\n":
         exit(84)
 
+def check_return_team(s, arg):
+    s.send(str.encode(arg.n + '\n'))
+    data = s.recv(4096)
+    try:
+        if data.decode('utf-8') == 'ko\n':
+            print('recv ko went team was send', file=stderr)
+            exit(84)
+    except:
+        print('recv ko went team was send', file=stderr)
+        exit(84)
+    tab = data.decode('utf-8').split('\n')
+    info_ia.number = int(tab[0])
+    info_ia.map_size_x = int(tab[0].split(' ')[0])
+    info_ia.map_size_x = int(tab[0].split(' ')[1])
+
 def start_of_ia(s, arg):
     check_good_start(s)
-    s.send(str.encode(arg.n))
-    info_ia.number = int(s.recv(4096).decode('utf-8'))
-    print(str(info_ia.number))
+    check_return_team(s, arg).decode('utf-8')
